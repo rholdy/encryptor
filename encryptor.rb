75d7ec4e7b74e133ed092de358f0b080c4c39308
DevPoint 1 <devpoint1@DevPoints-iMac-3.local>
2013-09-24 18:43:25 -0600

    Created a super advanced encrytion tool

---
+++ encryptor.rb
@@ -0,0 +1,30 @@
+class Encryptor	
+
+  def cipher
+    {'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
+     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
+     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
+     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
+     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
+     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
+     'y' => 'l', 'z' => 'm', ' ' => ' '}
+  end
+
+  def encrypt_string(string)
+    letters = string.split("")
+    results = letters.collect do |letter|
+      encrypt_letter(letter)
+    end
+    results.join
+  end
+
+  def encrypt_letter(letter)
+    lowercase_letter = letter.downcase
+    cipher[lowercase_letter]
+  end
+
+  def decrypt(string)
+    encrypt_string(string)
+  end
+
+end
